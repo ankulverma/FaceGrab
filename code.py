fe8fe0a587d62e5ccde6279017fd0de02b6612b9
UmeedChandel <46392692+UmeedChandel@users.noreply.github.com>
2021-06-07 08:05:58 +0530

    Clean Code

--- code.py
+++ code.py
@@ -21,24 +21,20 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 
 db = firebase.database()
 storage=firebase.storage()
-path_on_db = "/ImageSourceDirectory"
-path = 'ImageSourceDirectory'
-noww = datetime.datetime.now()
-datenow = noww.strftime('%Y-%m-%d')
-timenow = noww.strftime('%H:%M:%S')
+
+path_on_firebase = "/ImageSourceDirectory"
+path = 'Faces'
 
 images = []
 classRollnos = []
 myList = os.listdir(path)
-camname = { 0 :"Webcam", 1 : "Living Room" }
-print(myList)
+camname = { 0 :"Local Place", 1 : "Remote Place" }
 
 for cl in myList:
     curImg = cv2.imread(f'{path}/{cl}')
     images.append(curImg)
     classRollnos.append(os.path.splitext(cl)[0])
-print(classRollnos)
-
+print("[INFO: ImageSourceDirectory","Count=",len(myList),",ID's=",classRollnos,"]")
 
 def findEncodings(images):
     encodeList = []
@@ -48,6 +44,7 @@ def findEncodings(images):
         encodeList.append(encode)
     return encodeList
 
+#CODE DUMP
 #INSERT DATA db
 #data={'Name':"PRIYANSH VERMA",'Location':"webcam",'Date':"06/05/21",'Time':"09:21:24"}
 #db.child("csvTable").child("0206cs181118").set(data)
@@ -59,41 +56,36 @@ def findEncodings(images):
 #downloadlink=input("URL: ")
 #storage.child(downloadlink).download(img)
 
-#
 
 def timelocated(name, cap):
     noww = datetime.datetime.now()
     datenow = noww.strftime('%Y-%m-%d')
-    timenow = noww.strftime('%H:%M:%S')
+    timenow = noww.strftime('%I:%M:%S')
+
     personname = db.child("idTable").order_by_key().equal_to(name).get()
     for person in personname.each():
         studentname = person.val()['Name']
-    #data = {'Name': studentname, 'Location': camname.get(caplist.get(cap)), 'Date': datenow, 'Time': timenow}
+
     data = {'Name': studentname, 'Location': [camname.get(caplist.get(cap)),  datenow,  timenow]}
     db.child("csvTable").child(name).set(data)
     user = db.child("csvTable").get()
     users = user.val()
     val = False
     for i in users:
-        if i == name :
+        if i == name:
             val = True
+
     if val == True:
         db.child("csvTable").child(name).update({"Location" :[ camname.get(caplist.get(cap)) , datenow , timenow] })
-        #db.child("csvTable").child(name).update({"Date": datenow})
-        #db.child("csvTable").child(name).update({"Time": timenow})
-
     else:
         val =False
 
-
 encodeListKnown = findEncodings(images)
-print("Encoding Complete")
+print("[INFO: Encodings Completed]")
 
-
-# code for getting all the cameras starts here:
-index = 0  # starting index for the cameras : 0 being the webcam
-getcam = []  # empty list for the camera indexing
-caplist = {}  # a dictionary which will store the stream and the index
+index = 0  # starting index fo cameras : 0 being the webcam
+getcam = []  # empty list to store camera indexing
+caplist = {}  # empty dictionary to store video stream and the cam index
 while True:
     cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
     if not cap.read()[0]:
@@ -102,70 +94,47 @@ while True:
         getcam.append(index)
     cap.release()
     index += 1
-print(getcam)
-# the above code is for getting the cameras which are currently working in the machine
-# it checks the all the 0 to n ports for cameras and whichever port returns a camera
-# it gets stored in #getcam list Example = [0,1] where 0 is integrated webcam and 1 is external
 
 for value in getcam:
-
     current = cv2.VideoCapture(value)
     caplist[current] = value
-print(caplist)
+print("[INFO:",caplist,"]")
+
 # The above code generates a dictionary with key as the name of the current
 # stream of video capture with the index from getcam as a value
 # the key <VideoCapture 0000017290F2F3B0> is used in the below code as the actual stream
 # where as the value is used for the display of which cam is showing , 0 always being the integrated webcam
 
-
-# for cap in caplist:
 while True:
 
-    # cv2.imshow('Camera 1', cap0.read()[1])
-    # cv2.imshow('Camera' , cap1.read()[1])
-
     for cap in caplist:
-
-        # while True:
 
         success, img = cap.read()
         imgS = cv2.resize(img, (0, 0), None, 1, 1)
         imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
         facesCurFrame = face_recognition.face_locations(imgS)
-
         encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
         for (encodeFace, (top, right, bottom, left)) in zip(encodesCurFrame, facesCurFrame):
             matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
             faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-            # print(faceDis)
             matchIndex = np.argmin(faceDis)
             count = 0
+
             if matches[matchIndex]:
-
                 name = classRollnos[matchIndex]
-
-                # print(name)
-                # y1,x2,y2,x1 = faceLoc
-                # y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-
                 cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
                 y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(img, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75, (0, 255, 0), 2)
-
+                cv2.putText(img, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (0, 255, 0), 2)
                 timelocated(name, cap)
 
             else:
                 name = "Unknown"
-                # print(name)
-
-                # y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
                 cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
                 y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(img, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75, (0, 255, 0), 2)
+                cv2.putText(img, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,0.75, (0, 255, 0), 2)
 
         cv2.imshow(str(caplist.get(cap)), img)
-        # cv2.imshow('Webcam', img)
-        cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
